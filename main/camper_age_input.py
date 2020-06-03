c10ccc39668d01a6f4d58821f0ba5517753ed305
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-03 14:29:14 -0500

    Completed function convert_to_months

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -1,6 +1,23 @@
-def convert_to_months(x):
-    pass
+"""
+Program: camper_age_input.py
+Author: Daniel Meeker
+Date: 06/03/2020
+
+This program demonstrates Test Driven Development
+in Python with a simple function and unit test.
+"""
+
+
+def convert_to_months(years):
+    months = 12
+    months *= years
+    return months
 
 
 if __name__ == '__main__':
-    convert_to_months(x)
+    # get user input
+    age_in_years = input('Please enter your age (in years): ')
+    # call the function and save the result in a variable
+    age_in_months = convert_to_months(int(float(age_in_years)))
+    # print out the results
+    print(str(age_in_years) + ' years is equal to ' + str(age_in_months) + ' months.')
